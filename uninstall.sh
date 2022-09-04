f1a40df32143c101138c51b76e84599bfc98fc2a
Martin Butt <me@martinbutt.com>
2022-09-04 12:22:50 -0700

    Add all files to uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 echo "Uninstalling ddns"
+
 rm -f /usr/bin/inadyn
+rm -f /etc/init.d/S70inadyn
+rm -f /usr/lib/libconfuse.a
+rm -f /usr/lib/libconfuse.la
+rm -f /usr/lib/libconfuse.so
+rm -f /usr/lib/libconfuse.so.2
+rm -f /usr/lib/libconfuse.so.2.1.0
+
 echo "Uninstall complete"
