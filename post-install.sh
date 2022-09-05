fa69a102be32164536b95f1d70dc5a0016fc0306
Martin Butt <me@martinbutt.com>
2022-09-04 19:59:16 -0700

    Fixes

---
+++ post-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Running post install for fsck.ext"
+ln -s /usr/lib/libconfuse.so.2.1.0 /usr/lib/libconfuse.so
+ln -s /usr/lib/libconfuse.so.2.1.0 /usr/lib/libconfuse.so.2
+echo "Uninstall complete"
+
