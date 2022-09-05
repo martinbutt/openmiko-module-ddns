cb7cbc679a2f9dfe2270a6cff8c9f41b16d4c756
Martin Butt <me@martinbutt.com>
2022-09-04 20:09:19 -0700

    Fixes

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Running post install for fsck.ext"
+echo "Running post install for ddns"
 ln -s /usr/lib/libconfuse.so.2.1.0 /usr/lib/libconfuse.so
 ln -s /usr/lib/libconfuse.so.2.1.0 /usr/lib/libconfuse.so.2
 echo "Uninstall complete"
--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,7 @@ echo "Uninstalling ddns"
 
 rm -f /usr/bin/inadyn
 rm -f /etc/init.d/S70inadyn
+rm -f /etc/default/inadyn
 rm -f /usr/lib/libconfuse.a
 rm -f /usr/lib/libconfuse.la
 rm -f /usr/lib/libconfuse.so
