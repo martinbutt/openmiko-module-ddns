e1c5f966e34590fbc40daa16f7fd8e9af0b5ace1
Martin Butt <me@martinbutt.com>
2023-12-27 09:50:24 -0800

    Add service start

--- post-install.sh
+++ post-install.sh
@@ -3,5 +3,7 @@
 echo "Running post install for ddns"
 ln -s /usr/lib/libconfuse.so.2.1.0 /usr/lib/libconfuse.so
 ln -s /usr/lib/libconfuse.so.2.1.0 /usr/lib/libconfuse.so.2
-echo "Uninstall complete"
+/etc/init.d/S70inadyn start
+echo "Post install complete"
+
 
